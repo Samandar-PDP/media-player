39b52cf2a41128284ec8bb60129f0866d1d49abd
samandar-mee <samandarcoderdev@gmail.com>
2023-12-09 12:27:54 +0500

    tugap pole dedi

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -13,7 +15,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   late AudioPlayer _audioPlayer;
-  double _value = 0.0;
+  Duration maxDuration = const Duration(seconds: 0);
+  late ValueListenable<Duration> progress;
+  int _index = 0;
+  final _musicList = [
+    'dvrs',
+    'interworld',
+    'braz'
+  ];
 
   @override
   void initState() {
@@ -27,8 +36,16 @@ class _HomePageState extends State<HomePage> {
     super.dispose();
   }
 
+  getDuration() {
+    _audioPlayer.getDuration().then((value) {
+      maxDuration = value ?? const Duration(seconds: 0);
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    getDuration();
     return Scaffold(
       backgroundColor: Colors.indigoAccent,
       body: Padding(
@@ -40,38 +57,32 @@ class _HomePageState extends State<HomePage> {
             children: [
               Lottie.asset('assets/anim/anim_1.json',height: 250,width: 250, animate: _audioPlayer.state == PlayerState.playing),
               const SizedBox(height: 150),
-              Row(
-                children: [
-                  FutureBuilder(future: _audioPlayer.getCurrentPosition(), builder: (context, snapshot) {
-                    return Text("${snapshot.data?.inMinutes}:${snapshot.data?.inSeconds}",style: TextStyle(
-                        color: Colors.white
-                    ));
-                  }),
-                  Expanded(
-                    child: StreamBuilder(
-                      stream: _audioPlayer.onPositionChanged,
-                      builder: (context, snapshot) {
-                        return Slider(value: (snapshot.data?.inSeconds)?.toDouble() ?? 0.0, onChanged: (v) => setState(() {
-                          _audioPlayer.seek(snapshot.data ?? const Duration(microseconds: 0));
-                          setState(() {
-
-                          });
-                        }),activeColor: Colors.white,thumbColor: Colors.white);
+              StreamBuilder(
+                  stream: _audioPlayer.onPositionChanged,
+                  builder: (context, snapshot) {
+                    return ProgressBar(
+                      thumbColor: Colors.white,
+                      thumbGlowColor: Colors.white,
+                      progress: snapshot.data ?? const Duration(seconds: 0),
+                      total: maxDuration,
+                      onSeek: (duration) {
+                        _audioPlayer.seek(duration);
+                        setState(() {});
                       },
-                    ),
-                  ),
-                  FutureBuilder(future: _audioPlayer.getDuration(), builder: (context, snapshot) {
-                    return Text("${snapshot.data?.inMinutes}:${snapshot.data?.inSeconds}",style: TextStyle(
-                    color: Colors.white
-                    ));
-                  })
-                ],
-              ),
+                    );
+                  }),
               const SizedBox(height: 60),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.backward_end_alt),color: Colors.white,),
+                  IconButton(onPressed: () {
+                    if(_index > 0) {
+                      setState(() {
+                        _index--;
+                      });
+                    }
+                    _playOrStop();
+                  }, icon: const Icon(CupertinoIcons.backward_end_alt),color: Colors.white,),
                   Container(
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
@@ -80,8 +91,13 @@ class _HomePageState extends State<HomePage> {
                     child: IconButton(onPressed: _playOrStop, icon: Icon(_audioPlayer.state == PlayerState.playing ? CupertinoIcons.pause : CupertinoIcons.play,color: Colors.white)),
                   ),
                   IconButton(onPressed: () {
-
-                  }, icon: Icon(CupertinoIcons.forward_end_alt),color: Colors.white,),
+                    if(_index < _musicList.length) {
+                      setState(() {
+                        _index++;
+                      });
+                    _playOrStop();
+                    }
+                  }, icon: const Icon(CupertinoIcons.forward_end_alt),color: Colors.white,),
                 ],
               )
             ],
@@ -94,10 +110,11 @@ class _HomePageState extends State<HomePage> {
 
 
   void _playOrStop() async {
+    print(_index);
     if(_audioPlayer.state == PlayerState.playing) {
       await _audioPlayer.pause();
     } else {
-      await _audioPlayer.play(AssetSource('audio/interworld.mp3'));
+      await _audioPlayer.play(AssetSource('audio/${_musicList[_index]}.mp3'));
     }
     setState(() {});
   }
